6afd7116eb96a3a0979cb40ecd7644b1aac5a3e4
Katsuya Oba <katsuya.oba@gmail.com>
2019-02-10 16:37:58 +0900

    useEffect追加

--- src/Example.js
+++ src/Example.js
@@ -1,8 +1,16 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
-function Example() {
+//function Example() {
+const Example = () => {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0)
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    console.log('useEffect() is called.');
+    // Update the document title using the browser API
+    document.title = `You clicked ${count} times`
+  })
 
   return (
     <div>
